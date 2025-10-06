ac9f1e78ca38b2aa9a97528212483f99d514a4d4
Lê Nguyễn Phú Lợi <leloi.htn@gmail.com>
2025-10-06 13:17:39 +0700

    Add files via upload

---
+++ QuanLyLinhKien/QuanLyLinhKien/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuanLyLinhKien.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
