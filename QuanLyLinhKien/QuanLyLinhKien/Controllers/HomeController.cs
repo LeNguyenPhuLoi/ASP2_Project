5b0029bfee7bd7eef85adc3b5586def961c4297a
Lê Nguyễn Phú Lợi <leloi.htn@gmail.com>
2025-10-07 08:19:10 +0700

    Thêm 1 giao diện khác

--- QuanLyLinhKien/QuanLyLinhKien/Controllers/HomeController.cs
+++ QuanLyLinhKien/QuanLyLinhKien/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace QuanLyLinhKien.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult ViewCart()
+        {
+            return View();
+        }
     }
 }
