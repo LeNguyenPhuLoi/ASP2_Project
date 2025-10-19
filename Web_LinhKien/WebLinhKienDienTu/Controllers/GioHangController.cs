a46cff0365fbc8139d882495e98026968f28e868
LeNguyenVi1457 <23211tt3822@mail.tdc.edu.vn>
2025-10-19 18:14:29 +0700

    Thêm giao diện lần 1

---
+++ Web_LinhKien/WebLinhKienDienTu/Controllers/GioHangController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebLinhKienDienTu.Controllers
+{
+    public class GioHangController : Controller
+    {
+        public IActionResult GioHang()
+        {
+            return View();
+        }
+
+        public IActionResult ChiTietHoaDon()
+        {
+            return View();
+        }
+
+        public IActionResult LichSuMuaHang()
+        {
+            return View();
+        }
+    }
+}
