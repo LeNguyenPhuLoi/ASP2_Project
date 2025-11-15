bfef3e5951a5ff199957ee610305199581f1641f
LeNguyenVi1457 <23211tt3822@mail.tdc.edu.vn>
2025-11-15 10:33:29 +0700

    Update Chi Tiet Hoa Don

---
+++ Web_LinhKien/WebLinhKienDienTu/Repository/ChiTietHoaDonService.cs
@@ -0,0 +1,48 @@
+﻿using WebLinhKienDienTu.Models;
+using WebLinhKienDienTu.ViewModels;
+
+namespace WebLinhKienDienTu.Repository
+{
+    public class ChiTietHoaDonService : IChiTietHoaDonService
+    {
+        private readonly QllkContext _db;
+
+        public ChiTietHoaDonService(QllkContext db)
+        {
+            _db = db;
+        }
+
+        public ChiTietHoaDonViewModel LayChiTietHoaDon(string mahd)
+        {
+            var chitiet = (from hd in _db.Hoadons
+                           join cthd in _db.Chitiethoadons
+                               on hd.Mahd equals cthd.Mahd
+                           join sp in _db.Sanphams
+                               on cthd.Masp equals sp.Masp
+                           join kh in _db.Khachhangs
+                               on hd.Makh equals kh.Makh
+                           join nv in _db.Nhanviens
+                               on hd.Manv equals nv.Manv
+                           join khosp in _db.KhoSanphams
+                               on sp.Masp equals khosp.Masp
+                           join kho in _db.Khohangs
+                               on khosp.Makho equals kho.Makho
+                           where hd.Mahd == mahd
+                           select new ChiTietHoaDonViewModel
+                           {
+                               Mahd = hd.Mahd,
+                               Tenkh = kh.Tenkh,
+                               Ngaylap = hd.Ngaylap ?? DateTime.MinValue,
+                               TenNV = nv.Tennv,
+                               Trangthai = hd.Trangthai,
+                               Tensp = sp.Tensp,
+                               Tenkho = kho.Tenkho,
+                               Soluong = cthd.Soluong ?? 0,
+                               Dongia = sp.Dongia ?? 0
+                           }).FirstOrDefault();
+
+            return chitiet;
+        }
+    }
+
+}
---
+++ Web_LinhKien/WebLinhKienDienTu/ViewModels/ChiTietHoaDonViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace WebLinhKienDienTu.ViewModels
+{
+    public class ChiTietHoaDonViewModel
+    {
+        public string Mahd { get; set; } //từ HOADON
+        public string Tenkh { get; set; } //từ KHACHHANG
+        public DateTime Ngaylap { get; set; } //từ HOADON
+        public string TenNV { get; set; } //từ NHANVIEN
+        public string Trangthai { get; set; } // từ HOADON
+        public string Tensp { get; set; } //từ SANPHAM
+        public string Tenkho { get; set; } //từ KHOHANG
+        public int Soluong { get; set; } //từ CHITIETHOADON
+        public decimal Dongia { get; set; } //từ SANPHAM
+    }
+}
